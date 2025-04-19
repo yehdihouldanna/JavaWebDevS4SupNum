121692de8d47e119d00d8852ba1d3da229d19929
Yehdhih ANNA <yehdihouldanna@gmail.com>
2025-04-19 08:42:25 +0000

    session 10 : Swagger UI integartion

--- src/main/java/com/example/session3_dynamic_data_modelview/jwtModule/configurations/SecurityConfig.java
+++ src/main/java/com/example/session3_dynamic_data_modelview/jwtModule/configurations/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         //.requestMatchers("/auth/**", "/students/**", "/products/**").permitAll()  // 👈 public routes  
                         .anyRequest().authenticated()
                 )
